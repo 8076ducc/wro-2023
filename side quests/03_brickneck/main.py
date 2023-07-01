f264a4938ce3ca5630bf4c670efe1fe898bf2a13
neohaojun <neohaojun@gmail.com>
2023-07-01 10:05:00 +0800

    complete working program (requires tuning)

--- side quests/03_brickneck/main.py
+++ side quests/03_brickneck/main.py
@@ -5,103 +5,57 @@ from devices import *
 from pid import *
 from constants import *
 
-def see_line(thr, left_sensor = left_cs_f, right_sensor = right_cs_f):
-    passed = 0
-
-    if (left_sensor.rgb()[0] > BLACK_RGB_LEFT[0] + 5 and right_sensor.rgb()[0] > BLACK_RGB_RIGHT[0] + 5):
-        passed += 1
-
-    if passed == thr:
-        return True
-    else:
-        return False
-
 while True:
-    # line track from 0 to 1
-    LineTrack.track(
-        1500,
-        [
-            ((BLACK_RGB_LEFT[0] + WHITE_RGB_LEFT[0]) / 2),
-            ((BLACK_RGB_LEFT[1] + WHITE_RGB_LEFT[1]) / 2),
-            ((BLACK_RGB_LEFT[2] + WHITE_RGB_LEFT[2]) / 2),
-        ],
-        [
-            ((BLACK_RGB_RIGHT[0] + WHITE_RGB_RIGHT[0]) / 2),
-            ((BLACK_RGB_RIGHT[1] + WHITE_RGB_RIGHT[1]) / 2),
-            ((BLACK_RGB_RIGHT[2] + WHITE_RGB_RIGHT[2]) / 2),
-        ],
-        see_line(1),
+    line_track.track(
+        800,
+        # (9 + 68) / 2,
+        95,
+        95,
+        1,
     )
-
     base.brake()
 
-    # turn towards 2
-    base.run_target(
-        1500,
-        1500,
-        -180,
-        180,
+    line_track.track(
+        -800,
+        70,
+        70,
+        1,
+        left_sensor=left_cs_b,
+        right_sensor=right_cs_b,
     )
-
-    # move towards 2 (backwards)
-    base.run_target(
-        1500,
-        1500,
-        -1000,
-        -1000,
-    )
-
-    # turn towards 3
-    base.run_target(
-        1500,
-        1500,
-        -180,
-        180,
-    )
-
-    # line track from 2 to 3
-    LineTrack.track(
-        1500,
-        [
-            ((BLACK_RGB_LEFT[0] + WHITE_RGB_LEFT[0]) / 2),
-            ((BLACK_RGB_LEFT[0] + WHITE_RGB_LEFT[0]) / 2),
-            ((BLACK_RGB_LEFT[0] + WHITE_RGB_LEFT[0]) / 2),
-        ],
-        [
-            ((BLACK_RGB_RIGHT[0] + WHITE_RGB_RIGHT[0]) / 2),
-            ((BLACK_RGB_RIGHT[0] + WHITE_RGB_RIGHT[0]) / 2),
-            ((BLACK_RGB_RIGHT[0] + WHITE_RGB_RIGHT[0]) / 2),
-        ],
-        see_line(3),
-    )
-
     base.brake()
-    wait(500)
+    wait(50)
 
-    # line track from 3 to 0 (backwards)
-    LineTrack.track(
-        1500,
-        [
-            ((BLACK_RGB_LEFT[0] + WHITE_RGB_LEFT[0]) / 2),
-            ((BLACK_RGB_LEFT[0] + WHITE_RGB_LEFT[0]) / 2),
-            ((BLACK_RGB_LEFT[0] + WHITE_RGB_LEFT[0]) / 2),
-        ],
-        [
-            ((BLACK_RGB_RIGHT[0] + WHITE_RGB_RIGHT[0]) / 2),
-            ((BLACK_RGB_RIGHT[0] + WHITE_RGB_RIGHT[0]) / 2),
-            ((BLACK_RGB_RIGHT[0] + WHITE_RGB_RIGHT[0]) / 2),
-        ],
-        see_line(2, left_cs_b, right_cs_b),
-        left_cs_b,
-        right_cs_b,
+    base.run_target(1500, 1500, 0, -360)
+    wait(50)
+
+    line_track.track(
+        800,
+        95,
+        95,
+        1,
     )
-
     base.brake()
+    wait(100)
 
-    #turn to face 1
-    base.run_target(
-        1500,
-        1500,
-        0,
-        -360,
+    line_track.track(
+        -800,
+        70,
+        70,
+        3,
+        left_sensor=left_cs_b,
+        right_sensor=right_cs_b,
     )
+    base.brake()
+    wait(50)
+
+    line_track.track(
+        800,
+        95,
+        95,
+        2,
+    )
+    base.brake()
+    wait(50)
+
+    base.run_target(1500, 1500, 0, 360)
