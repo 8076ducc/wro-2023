b26ea43ae73697181abf7665d0b4e7eb2938e958
neohaojun <neohaojun@gmail.com>
2023-07-01 10:04:40 +0800

    implement pid accel

--- side quests/03_brickneck/pid.py
+++ side quests/03_brickneck/pid.py
@@ -1,12 +1,5 @@
+from pybricks.tools import wait
 from devices import *
-
-def abscap(x, cap):
-    if (x > abscap):
-        return abscap
-    elif (x < -abscap):
-        return -abscap
-    else:
-        return x
 
 class PID(object):
     def __init__(
@@ -21,15 +14,18 @@ class PID(object):
         self.correction = 0
         self.left=0,
         self.right=0,
-        self.loop = 0
+        self.current_speed = 0
+        self.lines_passed = 0
 
     def reset_values(self):
         self.proportional = 0
         self.integral = 0
         self.derivative = 0
         self.last_error = 0
-        self.loop = 0
+        self.current_speed = 0
+        self.lines_passed = 0
 
+        
 class LineTrack(PID):
     def __init__(self):
         PID.__init__(self)
@@ -43,40 +39,39 @@ class LineTrack(PID):
         left_sensor = left_cs_f,
         right_sensor = right_cs_f,
         reset=True,
-        kp=0,
+        kp=1.5,
         ki=0,
-        kd=0,
-        slew=0,
+        kd=20,
     ):
+        self.accel = 25
+
         if reset == True:
             self.reset_values()
 
-        while condition():
+        while self.lines_passed < condition:
+            if (left_sensor.reflection() < 20 and right_sensor.reflection() < 20):
+                self.lines_passed += 1
+                if self.lines_passed < condition:
+                    wait(50)
+
             self.error = (
-                - (left_th[0] - left_sensor.rgb()[0])
-                - (left_th[1] - left_sensor.rgb()[1])
-                - (left_th[2] - left_sensor.rgb()[2])
-                + (right_th[0] - right_sensor.rgb()[0])
-                + (right_th[1] - right_sensor.rgb()[1])
-                + (right_th[2] - right_sensor.rgb()[2])
+                - (left_th - left_sensor.reflection())
+                + ((right_th - right_sensor.reflection()))
             )
             self.proportional = self.error * kp
             self.integral += self.error
             self.derivative = (self.error - self.last_error) * kd
 
-            self.correction = (self.integral * ki) + self.proportional + self.derivative
+            self.correction = self.proportional + (self.integral * ki) + self.derivative
 
-            left_change = (speed + self.correction) - self.left
-            self.left += abscap(left_change, slew)
-            right_change = (speed - self.correction) - self.right
-            self.right += abscap(right_change, slew)
+            if self.current_speed < abs(speed): 
+                self.current_speed += self.accel * 0.1
 
             self.base.run(
-                self.left,
-                self.right,
+                (speed + (self.correction)) * (self.current_speed / abs(speed)),
+                (speed - (self.correction))  * (self.current_speed / abs(speed)),
             )
 
-            self.loop += 1
             self.last_error = self.error
 
 line_track = LineTrack()
